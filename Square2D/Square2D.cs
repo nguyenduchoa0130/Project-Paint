21c2560f64bb08c23449fda9aa4c83dede9044ad
nguyenduchoa0130 <nguyenduchoa0130@gmail.com>
2023-12-27 06:49:53 +0700

    create square 2D

---
+++ Square2D/Square2D.cs
@@ -0,0 +1,15 @@
+using Contract;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Square2D
+{
+    public class Square2D : CShape, IShape
+    {
+
+        
+    }
+}
