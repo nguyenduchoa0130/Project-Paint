197e075bdef960cd7638ba6804b9072fe6666a03
nguyenduchoa0130 <nguyenduchoa0130@gmail.com>
2023-12-28 07:14:35 +0700

    create circle 2D

---
+++ Circle2D/Circle2D.cs
@@ -0,0 +1,15 @@
+using Contract;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Circle2D
+{
+    public class Circle2D : CShape, IShape
+    {
+
+        
+    }
+}
