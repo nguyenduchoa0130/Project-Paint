3c0c3b5208ffb1f8493a671d340b95fe626cc462
nguyenduchoa0130 <nguyenduchoa0130@gmail.com>
2023-12-30 21:15:15 +0700

    Draw Rectangle 2D

--- Rectangle2D/Rectangle2D.cs
+++ Rectangle2D/Rectangle2D.cs
@@ -22,6 +22,48 @@ namespace Rectangle2D
             _leftTop = new Point2D() { X = x, Y = y };
         }
 
-       
+        public void HandleEnd(double x, double y)
+        {
+            _rightBottom = new Point2D() { X = x, Y = y };
+        }
+
+        public UIElement Draw(SolidColorBrush brush, int thickness, DoubleCollection dash)
+        {
+            var left = Math.Min(_rightBottom.X, _leftTop.X);
+            var top = Math.Min(_rightBottom.Y, _leftTop.Y);
+
+            var right = Math.Max(_rightBottom.X, _leftTop.X);
+            var bottom = Math.Max(_rightBottom.Y, _leftTop.Y);
+
+            var width = right - left;
+            var height = bottom - top;
+
+            var rect = new Rectangle()
+            {
+                Width = width,
+                Height = height,
+                StrokeThickness = thickness,
+                Stroke = brush,
+                StrokeDashArray = dash
+            };
+
+            Canvas.SetLeft(rect, left);
+            Canvas.SetTop(rect, top);
+
+            RotateTransform transform = new RotateTransform(this._rotateAngle);
+            transform.CenterX = width * 1.0 / 2;
+            transform.CenterY = height * 1.0 / 2;
+
+            rect.RenderTransform = transform;
+
+            return rect;
+        }
+
+        public IShape Clone()
+        {
+            return new Rectangle2D();
+        }
+
+        
     }
 }
